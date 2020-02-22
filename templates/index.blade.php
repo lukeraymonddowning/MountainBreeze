9071c24112bc69e79f7f2f3e78c6c7cefd399def
Luke Downing <luke@ricorocks.agency>
2020-02-22 12:38:25 +0000

    Example now include Alpine functionality

--- templates/index.blade.php
+++ templates/index.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 
-    <div class="flex items-center h-screen bg-gray-300">
-        <div class="w-full">
-            <h1 class="text-center text-6xl font-mono">Build Something Amazing</h1>
+    <div class="flex items-center h-screen bg-blue-900 bg-example">
+        <div class="max-w-5xl m-auto bg-white rounded-lg w-3/4 h-64 flex items-center shadow-xl" x-data="{ showPhrase: true }">
+            <h1 class="w-full text-center text-3xl md:text-5xl font-mono" @mouseover="showPhrase = false" @mouseout="showPhrase = true" x-text="showPhrase ? 'Build Something Amazing...' : '...With Mountain Breeze'"></h1>
         </div>
     </div>
 
