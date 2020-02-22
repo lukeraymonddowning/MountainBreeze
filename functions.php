1d53acc8de296ad283ec15eb002dd56a63249479
Luke Downing <luke@ricorocks.agency>
2020-02-22 11:15:51 +0000

    Simplified functions file

--- functions.php
+++ functions.php
@@ -1,22 +1,9 @@
 <?php
 
-
-/**
- * -------------------------------------------------------------------------
- * WELCOME
- * -------------------------------------------------------------------------
- * Thank you for using Mountain Breeze. We hope you find it a pleasure to use.
- * We've tried our hardest to make things as simple as possible, although you'll
- * note that there is no style.css file included in the repo.
- *
- * To get it, run `npm install` from the theme directory, followed by `npm run dev`.
- * For further information, checkout the README or visit https://github.com/lukeraymonddowning/MountainBreeze
- */
-
 /**
  * In order to avoid cluttering your functions file, we include our own here at the top.
- * This file is nothing special. It removes jQuery and enqueues your styles
- * (including Tailwind CSS) and scripts (including Alpine JS)
+ * This file is responsible for enqueuing your styles (including Tailwind),
+ * scripts (including Alpine), and enabling Blade templating support.
  */
 require_once __DIR__ . '/mountainbreeze/functions.php';
 
--- mountainbreeze/functions.php
+++ mountainbreeze/functions.php
@@ -12,17 +12,13 @@ use Illuminate\View\Engines\EngineResolver;
 use Illuminate\View\Engines\CompilerEngine;
 use Illuminate\View\Compilers\BladeCompiler;
 
-function mountain_breeze_enqueue()
-{
+add_action('wp_enqueue_scripts', function () {
     wp_deregister_script('jquery');
     wp_enqueue_style('style', get_stylesheet_uri());
     wp_enqueue_script('app', get_stylesheet_directory_uri() . '/dist/app.js');
-}
+});
 
-add_action('wp_enqueue_scripts', 'mountain_breeze_enqueue');
-
-function mountain_breeze_load_blade_view($template)
-{
+add_filter('template_include', function ($template) {
     $templateName = wp_basename($template, '.php');
     if (getBladeViewFactory()->exists($templateName)) {
         $GLOBALS['template_name'] = $templateName;
@@ -30,28 +26,26 @@ function mountain_breeze_load_blade_view($template)
     }
 
     return $template;
-}
-
-add_filter('template_include', 'mountain_breeze_load_blade_view');
+});
 
 function getBladeViewFactory()
 {
     $filesystem = new Filesystem;
-    $eventDispatcher = new Dispatcher(new Container);
-
     $viewResolver = new EngineResolver;
-    $bladeCompiler = new BladeCompiler($filesystem, getCompiledTemplateDirectory());
 
-    $viewResolver->register('blade', function () use ($bladeCompiler) {
-        return new CompilerEngine($bladeCompiler);
+    $viewResolver->register('blade', function () use ($filesystem) {
+        return new CompilerEngine(new BladeCompiler($filesystem, getCompiledTemplateDirectory()));
     });
 
     $viewResolver->register('php', function () {
         return new PhpEngine;
     });
 
-    $viewFinder = new FileViewFinder($filesystem, getTemplateDirectory());
-    return new Factory($viewResolver, $viewFinder, $eventDispatcher);
+    return new Factory(
+        $viewResolver,
+        new FileViewFinder($filesystem, getTemplateDirectory()),
+        new Dispatcher(new Container)
+    );
 }
 
 function getTemplateDirectory()
