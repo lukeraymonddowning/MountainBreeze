950dfe59b303af90866a7eb81c6c0c0bf794107c
Luke Downing <luke@ricorocks.agency>
2020-02-23 12:07:29 +0000

    Removed 3rd party collection lib

--- templates/index.blade.php
+++ templates/index.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-
     <div class="flex items-center h-screen bg-blue-900 bg-example">
         <div class="max-w-5xl m-auto bg-white rounded-lg w-3/4 h-64 flex items-center shadow-xl" x-data="{ showPhrase: true }">
             <h1 class="w-full text-center text-3xl md:text-5xl font-mono" @mouseover="showPhrase = false" @mouseout="showPhrase = true" x-text="showPhrase ? 'Build Something Amazing...' : '...With Mountain Breeze'"></h1>
         </div>
     </div>
-
 @endsection
