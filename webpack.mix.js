7d06a5130f998c78d6d2b5deb8dfd48749d44089
Matt <matt@Matt-MacBook.lan>
2020-08-06 22:03:06 +0100

    Remove double quotes in HTML lang

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,14 @@
-let mix = require('laravel-mix');
-let purgecss = require('@fullhuman/postcss-purgecss')
+let mix = require("laravel-mix");
+let purgecss = require("@fullhuman/postcss-purgecss");
 
-mix.js(`src/scripts/app.js`, 'dist/')
-    .postCss(`src/styles/style.css`, './', [
-        require('tailwindcss'),
-        ...process.env.NODE_ENV === 'production'
-            ? [purgecss({
-                content: ['**/*.php', '**/*.html'],
-                defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-            })] : []
-    ])
+mix.js(`src/scripts/app.js`, "dist/").postCss(`src/styles/style.css`, "./", [
+  require("tailwindcss"),
+  ...(process.env.NODE_ENV === "production"
+    ? [
+        purgecss({
+          content: ["**/*.php", "**/*.html"],
+          defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        }),
+      ]
+    : []),
+]);
