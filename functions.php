8726f525fe07062d16ffb4cfaca60437fb596926
luke <lukeraymonddowning@gmail.com>
2021-04-12 17:39:24 +0100

    Updates tailwind, alpine and blade dependencies.

--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * In order to avoid cluttering your functions file, we include our own here at the top.
- * This file is responsible for enqueuing your styles (including Tailwind),
- * scripts (including Alpine), and enabling Blade templating support.
+ * In order to avoid cluttering your functions file, we include our file here at
+ * the top. It is responsible for enqueuing our styles (such as Tailwind CSS),
+ * script files (including Alpine), and enabling Blade templating support.
  */
 require_once __DIR__ . '/mountainbreeze/functions.php';
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  purge: [
+      "**/*.blade.php",
+      "**/*.html"
+  ],
   theme: {
     extend: {},
   },
